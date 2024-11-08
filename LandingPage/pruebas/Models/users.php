8bce1f93b02cdb8bbe8136dcbbcf5a011b2703e5
AngelGameros <gvanny.gameros@gmail.com>
2024-11-08 08:48:17 -0800

    Getters y Setters en users.php

--- LandingPage/pruebas/Models/users.php
+++ LandingPage/pruebas/Models/users.php
@@ -1,21 +1,37 @@
 <?php
-  class User{
+class User {
     private $id_user;
-    private $user_name; 
+    private $user_name;
     private $user_password;
-    
 
-     
-    private static function holaMundo(){
+    // Getter para id_user
+    public function getIdUser() {
+        return $this->id_user;
+    }
 
+    // Setter para id_user
+    public function setIdUser($id_user) {
+        $this->id_user = $id_user;
+    }
 
+    // Getter para user_name
+    public function getUserName() {
+        return $this->user_name;
+    }
 
- }
+    // Setter para user_name
+    public function setUserName($user_name) {
+        $this->user_name = $user_name;
+    }
 
+    // Getter para user_password
+    public function getUserPassword() {
+        return $this->user_password;
+    }
 
-
+    // Setter para user_password
+    public function setUserPassword($user_password) {
+        $this->user_password = $user_password;
+    }
 }
-
-
-
 ?>
