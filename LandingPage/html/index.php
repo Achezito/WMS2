b9814b1cadb6f72594c9768286743b0b019737b0
AngelGameros <gvanny.gameros@gmail.com>
2024-11-14 18:46:13 -0800

    Cambios en mi rama

--- LandingPage/html/index.php
+++ LandingPage/html/index.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Límite de inactividad en segundos (por ejemplo, 10 minutos = 600 segundos)
-$limite_inactividad = 10;
+$limite_inactividad = 28800;
 
 // Verificar el tiempo de inactividad
 if (isset($_SESSION['ultimo_acceso'])) {
@@ -31,31 +31,32 @@ if (!isset($_SESSION['worker_user'])) {
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Principal</title>
-    <link rel="stylesheet" href="/LandingPage/css/index.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link href="../css/fontawesome/fontawesome.css" rel="stylesheet" />
+    <link href="../css/fontawesome/solid.css" rel="stylesheet" />
     <script src="../js/index.js"></script>
 </head>
-<body>
 
+<body>
     <header>
         <div id="header-left">
             <div id="header-menu" onclick="toggleMenu()">
                 <i class="fa fa-bars"></i>
             </div>
             <div id="header-logo">
-                <img src="../img/Logos/LineLogo.png" >
+                <img src="../img/Logos/LineLogo.png">
             </div>
             <h1>CISTA</h1>
-            <h1>
-                Bienvenido <?php echo htmlspecialchars($_SESSION['worker_user']) . ' con id: ' . htmlspecialchars($_SESSION['personal_id'] .
-                ' y correo: '. htmlspecialchars($_SESSION['email'])); ?>
-            </h1>
         </div>
         <div id="header-right">
+            <h1 id="welcome-msg">
+                <?php echo htmlspecialchars($_SESSION['worker_user']); ?>
+            </h1>
             <div id="user-photo">
                 <img src="../img/Users/User.jpg" alt="User Photo">
             </div>
@@ -69,21 +70,34 @@ if (!isset($_SESSION['worker_user'])) {
     <!-- Menú lateral -->
     <div id="menu">
         <ul>
-            <li><i class="fas fa-home"></i><a href="#"> Home</a></li>
-            <li><i class="far fa-user"></i><a href="#"> My account</a></li>
-            <li><i class="far fa-clipboard"></i><a href="#" id="prestamos-link"> Préstamos </a></li>
+            <li><i class="fas fa-home"></i><a href="index.php"> Home</a></li>
+            <li><i class="fas fa-user"></i><a href="#"> My account</a></li>
+            <li><i class="fas fa-clipboard"></i><a href="#" id="prestamos-link"> Préstamos </a></li>
         </ul>
     </div>
-    
+
     <!-- División para los cinco botones en forma de cartas -->
     <div id="button-cards-container">
         <div class="button-card"><i class="fas fa-home"></i> Inicio</div>
-        <div class="button-card"><i class="fas fa-search"></i> Materiales</div>
+        <a href="materials.php" style="text-decoration: none; color: inherit;">
+            <div class="button-card">
+                <i class="fas fa-search"></i> Materiales
+            </div>
+        </a>
         <div class="button-card"><i class="fas fa-file-alt"></i> Formularios</div>
-        <div class="button-card"><i class="fas fa-user"></i> Usuarios</div>
-        <div class="button-card"><i class="fas fa-clock"></i> Historiales</div>
+        <a href="users.php" style="text-decoration: none; color: inherit;">
+            <div class="button-card">
+                <i class="fas fa-user"></i> Usuarios
+            </div>
+        </a>
+        <a href="history.php" style="text-decoration: none; color: inherit;">
+            <div class="button-card">
+                <i class="fas fa-clock"></i> Historiales
+            </div>
+        </a>
+
     </div>
-    
+
     <!-- División para las cuatro cartas de contenido -->
     <div id="cards-container">
         <div class="card">Movimientos recientes en los materiales</div>
@@ -92,4 +106,5 @@ if (!isset($_SESSION['worker_user'])) {
         <div class="card">Próximos equipos de recibir mantenimiento</div>
     </div>
 </body>
+
 </html>
---
+++ LandingPage/html/users.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+
+// Verificar si el usuario ha iniciado sesión
+if (!isset($_SESSION['worker_user'])) {
+    header('location: /WMS2/LandingPage/html/login.php');
+    exit();
+}
+
+// Actualizar el tiempo de último acceso
+$_SESSION['ultimo_acceso'] = time();
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Historiales</title>
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/users.css">
+    <link href="../css/fontawesome/fontawesome.css" rel="stylesheet" />
+    <link href="../css/fontawesome/solid.css" rel="stylesheet" />
+    <script src="../js/index.js"></script>
+</head>
+
+<body>
+    <header>
+        <div id="header-left">
+            <div id="header-menu" onclick="toggleMenu()">
+                <i class="fa fa-bars"></i>
+            </div>
+            <div id="header-logo">
+                <img src="../img/Logos/LineLogo.png">
+            </div>
+            <h1>Usuarios</h1>
+        </div>
+        <div id="header-right">
+            <h1 id="welcome-msg">
+                <?php echo htmlspecialchars($_SESSION['worker_user']); ?>
+            </h1>
+            <div id="user-photo">
+                <img src="../img/Users/User.jpg" alt="User Photo">
+            </div>
+            <div id="header-logos">
+                <i class="fas fa-cog"></i>
+                <i class="fas fa-sign-out-alt" id="logout-icon"></i>
+            </div>
+        </div>
+    </header>
+
+    <!-- Menú lateral -->
+    <div id="menu">
+        <ul>
+            <li><i class="fas fa-home"></i><a href="../html/index.php"> Home</a></li>
+            <li><i class="fas fa-user"></i><a href="#"> My account</a></li>
+            <li><i class="fas fa-clipboard"></i><a href="#" id="prestamos-link"> Préstamos </a></li>
+        </ul>
+    </div>
+
+    <!-- Recuadro grande para mostrar contenido -->
+    <div id="users-content">
+        <div class="content-box">
+            <h2>AQUI SE DIBUJARA LA LISTA DE USUARIOS</h2>
+        </div>
+    </div>
+
+</body>
+
+</html>
