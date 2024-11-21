83413760c16ccb3ce5617895ed4dd947bd4f03a7
Achezito <achezzini@hotmail.com>
2024-11-21 13:40:07 -0800

    ruta nueva en usaurio

--- LandingPage/phpFiles/config/process_login.php
+++ LandingPage/phpFiles/config/process_login.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         echo json_encode([
             "success" => true,
-            "redirect" => "/WMS2/LandingPage/html/users/usuario.php" 
+            "redirect" => "/WMS2/LandingPage/html/usuario/usuario.php" 
         ]);
         exit;
     } else {
