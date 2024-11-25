282607c89910d10424556929640b00e426175343
Achezito <achezzini@hotmail.com>
2024-11-25 11:02:32 -0800

    weas cambiadas

--- LandingPage/html/administrador/loginAdmin.php
+++ LandingPage/html/administrador/loginAdmin.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../../css/login.css">
 </head>
 
 <body>
@@ -18,7 +18,7 @@
             <div id="error" style="color: red;"></div>
 
             <!-- Formulario de login -->
-            <form id="loginForm" >
+            <form id="loginForm" action="tu_script.php" method="POST">
                 <div class="form-group">
                     <label for="name">Nombre de usuario</label>
                     <input type="text" id="name" name="username" required>
@@ -39,7 +39,7 @@
 
         <div class="left-panel">
             <div class="branding">
-                <img src="../img/Logos/LineLogo.png" alt="Logo" class="logo">
+                <img src="../../img/Logos/LineLogo.png" alt="Logo" class="logo">
                 <h1>Bienvenido a CISTA</h1>
             </div>
         </div>
@@ -47,7 +47,7 @@
     </div>
 
     <!-- Incluir el archivo JavaScript externo -->
-    <script src="../js/login.js" defer></script>
+    <script src="../../js/login.js" defer></script>
 </body>
 
 </html>
